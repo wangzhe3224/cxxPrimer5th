30fd4a3a6a35eceec7ca5955dae90479dcadfc7b
wangzhe3224 <wangzhetju@gmail.com>
2015-08-17 10:37:23 +0100

    ch13

---
+++ ch13/ex13_05.h
@@ -0,0 +1,29 @@
+//
+// ex13_05.h
+//
+// Created by wangzhe on 16 AGU, 2015
+// Copyright (c) 2015 wangzhe. All rights reserved.
+//
+// Given the following sketch of a class, write a
+// copy constructor that copies all the members.
+// Your constructor should dynamically allocate a new string and
+// copy the object to which ps points,
+// rather than copying ps itself.
+
+#ifndef EX13_05_H
+#define EX13_05_H
+
+#include <string>
+
+class HasPtr {
+ private:
+  std::string *ps;
+  int i;
+ public:
+ HasPtr(const std::string &s = std::string()):
+  ps(new std::string(s)), i(0) {}
+ HasPtr(const HasPtr &cp):
+  ps(new std::string(*cp.ps)), i(cp.i) {}
+};
+
+#endif
